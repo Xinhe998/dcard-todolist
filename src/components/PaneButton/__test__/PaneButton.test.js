64875ed0f1ba71c357ee3e72891c80949f7a4154
Xinhe <xinhe998@gmail.com>
2019-04-09 14:30:20 +0800

    setup environment scripts & config

---
+++ src/components/PaneButton/__test__/PaneButton.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { shallow } from 'enzyme';
+import PaneButton from '../index';
+
+describe('<PaneButton />', function () {
+
+})
